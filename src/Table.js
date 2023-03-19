9469b0e8623190d4c483ba4281e92df55b63b563
nzgamer41 <nzgamer41@gmail.com>
2023-03-19 20:59:47 +1300

    Fixed table generation

--- src/Table.js
+++ src/Table.js
@@ -1,23 +1,24 @@
 export default function Table({theadData, tbodyData}) {
-      return (
-        <table>
-        <thead>
-          <tr>
-            {theadData.map((heading, index) => (
-              <th key={index}>{heading}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-        {tbodyData[0].map((enemy, index) => {
+   const maxLength = Math.max(tbodyData[0].length, tbodyData[1].length);
+  return (
+    <table>
+      <thead>
+        <tr>
+          {theadData.map((heading) => {
+            return <th key={heading}>{heading}</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {[...Array(maxLength)].map((_, index) => {
           return (
             <tr key={index}>
-              <td>{enemy}</td>
-              <td>{tbodyData[1][index]}</td>
+              <td>{index < tbodyData[0].length ? tbodyData[0][index] : ""}</td>
+              <td>{index < tbodyData[1].length ? tbodyData[1][index] : ""}</td>
             </tr>
           );
         })}
-        </tbody>
-      </table>
-      );
+      </tbody>
+    </table>
+  );
     }
